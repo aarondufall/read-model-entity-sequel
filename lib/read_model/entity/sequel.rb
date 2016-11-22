f7dbb4bf13f8463a2f6238c20fe33ce8891b14c7
Nathan Ladd <nathan@obsidianexchange.com>
2016-11-22 15:30:53 -0600

    Database timezone is set to UTC

--- lib/read_model/entity/sequel.rb
+++ lib/read_model/entity/sequel.rb
@@ -10,3 +10,5 @@ require 'read_model/entity/sequel/connection'
 require 'read_model/entity/sequel/connection/adjusted_logger'
 require 'read_model/entity/sequel/connection/settings'
 require 'read_model/entity/sequel/sequel'
+
+Sequel.database_timezone = :utc
